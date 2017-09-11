5891ba3ff1a8b34a456ed94bf3f95da9d5c03713
macardiel <macardiel@miners.utep.edu>
2017-09-11 14:40:40 -0600

    worked on tokenizer

--- tokenizer/mytoc.C
+++ tokenizer/mytoc.C
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int countWords( char[] string ) {
-    
+#define IN 1 /* Inside a word */
+#define OUT 0 /* outside a 'd */
+
+int countWords( char[] str ) {
+    int n, c, nw, state;
+    state = OUT;
+    nw = 0;
+    for( n = 0; str[n]; n++ ) {
+        c = str[n];
+        if( c == ' ' || c == '\n' || c = '\t' )
+            state = OUT;
+        else if( state == OUT ) {
+            state = IN;
+            ++nw;
+        }
+    }
+    return nw;
 }
-char ** mytoc(char *str, char delim) {
+char ** mytoc( char *str, char delim ) {
+    int numWords = countWords( str );
+    int Index = 0;
+    char **tokens = calloc(tCount+2, sizeof(char*));
+    
+    char temp = ' ';
+    
+    int i;
+    for( i = 0; str[i]; i++ ) {
+        if( str[i] == ' ' ) {
+            temp = str[i];
+            str[i] = '\0';
+        }
+        else if( str[i] != '\0' && temp == ' ') {
+            tokens[ Index++ ] = &str[i];
+            temp = str[i];
+        }
+    }
+    
+    return tokens;
 }
