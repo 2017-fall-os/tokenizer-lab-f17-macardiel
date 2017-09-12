1bf7fed22814ed1b857d4dbfdbc85da3af9eccbf
macardiel <macardiel@miners.utep.edu>
2017-09-11 21:55:44 -0600

    semi-working

--- tokenizer/test.c
+++ tokenizer/test.c
@@ -19,33 +19,31 @@ void printTokenV( char **tVector ) {
 
 int main(int argc, char **argv ) {
     char **tokenV;
-    char *test1, *test2, *test3, *test4, *test5;
+    char test1[] = "Hello my dog's name is Darwin";
+    char test2[] = "     Hello   World!    ";
+    char test3[] = "";
+    char test4[] = "          ";
+    char test5[] = " HelloWorld";
     char input[100];
-    
-    *test1 = "Hello my dog's name is Darwin";
-    *test2 = "     Hello   World!    ";
-    *test3 = "";
-    *test4 = "          ";
-    *test5 = " HelloWorld";
         
-    printf( "%s:\n", *test1 );
-    tokenV = mytoc( *test1, ' ');
+    printf( "%s:\n", test1 );
+    tokenV = mytoc( test1, ' ');
     printTokenV( tokenV );
     
-    printf( "%s:\n", *test2 );
-    tokenV = mytoc( *test2, ' ');
+    printf( "%s:\n", test2 );
+    tokenV = mytoc( test2, ' ');
     printTokenV( tokenV );
     
-    printf( "%s:\n", *test3 );
-    tokenV = mytoc( *test3, ' ');
+    printf( "%s:\n", test3 );
+    tokenV = mytoc( test3, ' ');
     printTokenV( tokenV );
     
-    printf( "%s:\n", *test4 );
-    tokenV = mytoc( *test4, ' ');
+    printf( "%s:\n", test4 );
+    tokenV = mytoc( test4, ' ');
     printTokenV( tokenV );
     
-    printf( "%s:\n", *test5 );
-    tokenV = mytoc( *test5, ' ');
+    printf( "%s:\n", test5 );
+    tokenV = mytoc( test5, ' ');
     printTokenV( tokenV );
     
     printf("\n----------\n");
